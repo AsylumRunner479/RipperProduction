d97cf8e37cbc369f31faa3a9a27376f2b31f51db
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-09-10 14:43:22 +1000

    adding pathfinding

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    private GameObject player;
+    public Shader blue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+    //public static void SetGlobalVector(Blue, );
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
