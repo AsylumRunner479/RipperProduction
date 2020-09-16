b2f15714e50d69933e8370b28589611f728b47f7
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-09-16 16:05:29 +1000

    added proper player movement

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    private int height;
-    private int width;
+    public int height;
+    public int width;
     private float cellSize;
     private int[,] gridArray;
     Node[,] node;
-    
-   public void Setup(int width, int height, float cellSize, GameObject Building)
+    Transform[,] buildings;
+    public float moveSpeed = 0;
+    //Vector2Int currentNodeIndex
+    //Vector2Int nextNodeIndex
+    //Vector3.lerp(node[currentNodeIndex.x][currentNodeIndex.y], node[nextNodeIndex.x][nextNodeIndex.y]
+
+    public void GetPosition(Transform _transform, Vector2Int _currentIndex, Vector2Int _nextIndex, float _factor)
+    {
+        Vector3 start = buildings[_currentIndex.x, _currentIndex.y].position;
+        Vector3 end = buildings[_nextIndex.x, _nextIndex.y].position;
+        _transform.position = Vector3.Lerp(start, end, _factor);
+    }
+
+    public void Setup(int width, int height, float cellSize, GameObject Building)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
         gridArray = new int[width, height];
-        
+        buildings = new Transform[width, height];
+
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
             for (int y = 0; y < gridArray.GetLength(1); y++)
             {
                 Vector3 worldPoint = new Vector3(x * cellSize, 0, y * cellSize);
-                Instantiate(Building, worldPoint, Quaternion.identity);
-                
+                buildings[x,y] = Instantiate(Building, worldPoint, Quaternion.identity).transform;
             }
         }
     }
--- Assets/Scripts/PlayerUnit.cs
+++ Assets/Scripts/PlayerUnit.cs
@@ -8,25 +8,59 @@ public class PlayerUnit : MonoBehaviour
     public GameObject deathImage, victoryImage;
     public static bool isDead = false;
     public static bool HasWon = false;
-    public Vector3 targetPosition;
-    public float moveDistance;
-    public CharacterController controller;
+    public Vector2Int currentIndex,nextIndex;
+    [Tooltip("The amount of time it takes to move from one node to the next.")]
+    public float maxTime = 1;
+    //public CharacterController controller;
+    private int knife, map, medication;
+    public bool hasKnife, hasMap, hasMedication;
+    public GameObject[] inventory;
+    public Grid grid;
+    public GameManager game;
+
+    private float currentTime = 0;
     // Start is called before the first frame update
     void Start()
     {
-        targetPosition = transform.position;
-        CurSpeed = 1;
-        MaxSpeed = 1;
+        //targetPosition = transform.position;
+        CurSpeed = 2;
+        MaxSpeed = 2;
+        currentIndex = new Vector2Int(2, 1);
+        nextIndex = currentIndex;
+        currentTime = maxTime;
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "enemy")
+        if (other.tag == "enemy" && medication <= 1)
         {
-            Death();
+            if (knife >= 1)
+            {
+                Win();
+            }
+            else
+            {
+                Death();
+            }
+            
         }
         else if (other.tag == "Respawn")
         {
-
+            Win();
+        }
+        else if (other.tag == "medication")
+        {
+            hasMedication = true;
+            inventory[0].SetActive(true);
+        }
+        else if (other.tag == "Knife")
+        {
+            hasKnife = true;
+            inventory[1].SetActive(true);
+        }
+        else if (other.tag == "Map")
+        {
+            hasMap = true;
+            inventory[2].SetActive(true);
         }
     }
     private void Death()
@@ -44,47 +78,98 @@ public class PlayerUnit : MonoBehaviour
         
         
     }
+    private void powerDown()
+    {
+        knife--;
+        medication--;
+        map--;
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (medication <= 0)
+        {
+            inventory[0].SetActive(false);
+        }
+        if (knife <= 0)
+        {
+            inventory[1].SetActive(false);
+        }
+        if (map <= 0)
+        {
+            inventory[2].SetActive(false);
+        }
         if (GameManager.isPlayerTurn == false)
         {
             CurSpeed = MaxSpeed;
         }
+        
         else
         {
-            Debug.Log(Vector3.Distance(transform.position, targetPosition));
+            //Vector3 newPos = new Vector3((int)transform.position.x, (int)transform.position.y, (int)transform.position.z);
 
-            if (Vector3.Distance(transform.position, targetPosition) > Mathf.Epsilon)
+            //if (Vector2.Distance(currentIndex,nextIndex) > Mathf.Epsilon)
+            //{
+            //    grid.GetPosition(transform, currentIndex, nextIndex);
+
+            //}
+            // If we haven't moved completely, add time to this frame
+            if (currentTime < maxTime)
             {
-                transform.position = Vector3.Lerp(transform.position, targetPosition, 8 * Time.deltaTime);
-
+                currentTime += Time.deltaTime;
+                grid.GetPosition(transform, currentIndex, nextIndex, Mathf.Clamp01(currentTime / maxTime));
             }
             else
             {
-                if (Input.GetKeyDown(KeyCode.UpArrow))
+                if (CurSpeed <= 0)
                 {
-                    targetPosition += Vector3.forward * moveDistance;
-                    CurSpeed -= 1;
+                    powerDown();
+                    game.EndPlayerTurn();
                 }
-                else if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    targetPosition += Vector3.back * moveDistance;
-                    CurSpeed -= 1;
-                }
-                else if (Input.GetKeyDown(KeyCode.RightArrow))
-                {
-                    targetPosition += Vector3.right * moveDistance;
-                    CurSpeed -= 1;
-                }
-                else if (Input.GetKeyDown(KeyCode.LeftArrow))
-                {
-                    targetPosition += Vector3.left * moveDistance;
-                    CurSpeed -= 1;
-                }
-                else
-                {
 
+                //targetPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                if (Input.GetKeyDown(KeyCode.UpArrow) && nextIndex.y + 1 <= grid.height - 1)
+                {
+                    currentIndex = nextIndex;
+                    nextIndex.y++;
+                    currentTime = 0;
+                    CurSpeed -= 1;
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow) && nextIndex.y - 1 >= 0)
+                {
+                    currentIndex = nextIndex;
+                    nextIndex.y--;
+                    currentTime = 0;
+                    CurSpeed -= 1;
+                }
+                else if (Input.GetKeyDown(KeyCode.RightArrow) && nextIndex.x + 1 <= grid.width - 1)
+                {
+                    currentIndex = nextIndex;
+                    nextIndex.x++;
+                    currentTime = 0;
+                    CurSpeed -= 1;
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftArrow) && nextIndex.x + 1 >= 0)
+                {
+                    currentIndex = nextIndex;
+                    nextIndex.x--;
+                    currentTime = 0;
+                    CurSpeed -= 1;
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    medication = 3;
+                    hasMedication = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    knife = 1;
+                    hasKnife = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    map = 3;
+                    hasMap = false;
                 }
             }
         }
